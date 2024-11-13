9908c512f12e50992431cedb8edb80663ed862f0
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-11-13 10:15:48 -0500

    Can now mark items as completed or not, thanks to the Observation framework.

--- EnhancedTodoList/Models/TodoItem.swift
+++ EnhancedTodoList/Models/TodoItem.swift
@@ -5,9 +5,11 @@
 //  Created by Benjamin Lavallee on 2024-11-13.
 //
 
+import Observation
 import Foundation
  
-struct TodoItem: Identifiable {
+@Observable
+class TodoItem: Identifiable {
     
     let id = UUID()
     var details: String
