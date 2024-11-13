4a52a210ba7daea41b3bed15e1c226d57754229b
Benjamin Lavallee <blavallee@lcs.on.ca>
2024-11-13 09:59:16 -0500

    Added the initial data model.

---
+++ EnhancedTodoList/Views/TodoListView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoListView.swift
+//  EnhancedTodoList
+//
+//  Created by Benjamin Lavallee on 2024-11-13.
+//
+
+import SwiftUI
+
+struct TodoListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TodoListView()
+}
